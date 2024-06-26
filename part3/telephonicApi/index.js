883d4ec61fd77428764c4f7fd92e41dedb230119
Marcos <expositomarcos02@gmail.com>
2024-06-27 00:36:48 +0200

    Added 3.7

--- part3/telephonicApi/index.js
+++ part3/telephonicApi/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 // Middleware to parse JSON bodies
 app.use(express.json())
+app.use(morgan('tiny'))
 
 // Initial list of persons
 let persons = [
