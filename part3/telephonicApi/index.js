cd947d465463e425b3bf3e6e1f3686e2f7f49f73
Marcos <expositomarcos02@gmail.com>
2024-06-29 03:34:01 +0200

    Added 3.9

--- part3/telephonicApi/index.js
+++ part3/telephonicApi/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 // Middleware to parse JSON bodies
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', (req) => {
   return JSON.stringify(req.body)
@@ -98,7 +100,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 // Start the server
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
